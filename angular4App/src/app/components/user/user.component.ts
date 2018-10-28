a9dc2df942139ce7b16e46e64393bfb3f999f5f8
BrianARuff <thelonewolf198927@gmail.com>
2018-10-28 02:57:57 -0400

    CRD of CRUD opertaions added in thus far

---
+++ angular4App/src/app/components/user/user.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from "@angular/core";
+import Address from "./user-interfaces";
+
+@Component({
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"]
+})
+export class UserComponent implements OnInit {
+  name: string;
+  age: number;
+  email: string;
+  address: Address;
+  hobbies: string[];
+  isUpperCase: boolean;
+  constructor() {
+    console.log(`Constructor ran`);
+  }
+
+  ngOnInit() {
+    console.log(`ngOnInit ran`);
+    this.name = "Brian Ruff";
+    this.age = 29;
+    (this.email = "brff19@gmail.com"),
+      (this.address = {
+        street: "1116 N. Ransom St.",
+        city: "Gastonia",
+        state: "North Carolina"
+      });
+    this.hobbies = ["write code", "watch movies", "listen to music"];
+    this.isUpperCase = false;
+  }
+
+  onClick() {
+    if (this.isUpperCase) {
+      this.name = this.name.toLowerCase();
+      this.isUpperCase = !this.isUpperCase;
+    } else {
+      this.name = this.name.toUpperCase();
+      this.isUpperCase = !this.isUpperCase;
+    }
+    this.hobbies.push("Exercise");
+  }
+  
+  addHobby(hobby) {
+    // console.log(hobby);
+    this.hobbies.unshift(hobby);
+    return false;
+  }
+
+  deleteHobby(hobby, i) {
+    // console.log(hobby);
+    for(let i = 0; i < this.hobbies.length; i++) {
+      if(this.hobbies[i] === hobby) {
+        this.hobbies.splice(i, 1);
+        break;
+      }
+    }
+  }
+}
